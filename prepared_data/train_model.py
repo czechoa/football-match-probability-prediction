2c156fadcb0667d0dd2d6d99d1ea498bcba47cd4
czechoa <andrzej.czechu.ja@gmail.com>
2022-05-07 00:49:32 +0200

    dnn model results

--- prepared_data/train_model.py
+++ prepared_data/train_model.py
@@ -1,22 +1,23 @@
+from datetime import datetime
 from pathlib import Path
-import  re
+import re
 import tensorflow as tf
+import os
 
 
 def callbacks(model_name):
-    early_stop = tf.keras.callbacks.EarlyStopping(monitor='accuracy', verbose=1, patience=2)
+    early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', verbose=1, patience=5)
 
-    reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(monitor='accuracy', factor=0.25, patience=5,
+    reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.25, patience=5,
                                                      verbose=1, cooldown=0, min_lr=0.0001)
 
-    checkpoint_filepath = Path(f'saved_model/checkpoint/{model_name}')
+    checkpoint_filepath = Path(f'saved_model/checkpoint/final_result/' + datetime.now().strftime("%Y:%m:%d-%H:%M:%S") +'_epoch{epoch:02d}-loss{val_loss:.3f}-train{loss:.3f}.hdf5')
 
     checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
         filepath=checkpoint_filepath,
         save_weights_only=True,
-        monitor='accuracy',
-        mode='max',
-        save_best_only=True)
+        monitor='val_loss',
+        mode='min',)
 
     log_dir = Path(f'logs/fit/{model_name}')
 
@@ -31,32 +32,6 @@ def split_features_target_and_map_target(data):
     di = {'home': 2, 'draw': 1, 'away': 0}
     target = target.replace(di)
     return train_features, target
-
-
-def callbacks(model_name):
-    early_stop = tf.keras.callbacks.EarlyStopping(monitor='accuracy', verbose=1, patience=2)
-
-    reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(monitor='accuracy', factor=0.25, patience=5,
-                                                     verbose=1, cooldown=0, min_lr=0.0001)
-
-    checkpoint_filepath = Path(f'saved_model/checkpoint/{model_name}')
-
-    checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
-        filepath=checkpoint_filepath,
-        save_weights_only=True,
-        monitor='accuracy',
-        mode='max',
-        save_best_only=True)
-
-    log_dir = Path(f'logs/fit/{model_name}')
-
-    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
-
-    return early_stop, reduce_lr, tensorboard_callback, checkpoint_callback
-
-
-import os
-
 
 def save_results(output_path, output_string, model_summary=None):
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
